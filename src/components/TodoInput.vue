4880808802dfd5a5bc766c6c816ce4d56fdb0e31
songcs <songcs0329@gmail.com>
2021-06-29 22:31:12 +0900

    210627 뷰 컴포넌트 수정 todoinput...

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo"/>
+    <span class="addContainer" @click="addTodo">
+      <i class="fas fa-plus addBtn"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    }
+  },
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+  background-color: #fff;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  display: block;
+  width: 3rem;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: #fff;
+  vertical-align: middle;
+}
+</style>
